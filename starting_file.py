a71fd6b9e06d77fb4d88bf9028a37e15bcebb917
UmbraSky <rks023@email.latech.edu>
2023-04-19 14:04:37 -0500

    Starting thing with header like our lord Josh taught us

---
+++ starting_file.py
@@ -0,0 +1,6 @@
+####################################################################################
+# Team Name: Texas Toast
+# Members: Ryan Saffel, Brie Bays, Austin Newsom, Elora Browning
+# Date: 4/19/2023
+# Description: Hidden Security Camera
+###################################################################################
