8b3d96a1679e5db1ecf30a7a0c1bbfcc32277b3d
AustinNewsom <118396628+AustinNewsom@users.noreply.github.com>
2023-04-24 14:36:49 -0500

    Update starting_file.py
    Additional Comments for personal reference

--- starting_file.py
+++ starting_file.py
@@ -13,13 +13,31 @@
 
 # Code that will activate the green light whenever the program begins running
 
+    # Print message to the terminal saying "Program Started"
 
+    # Code to play a sound when green light activates
 
-# Code that will activate the red light when invalid user input is given
+    # Code for when the green button is pushed
 
+        # "Manual System Restart" message when button is pushed
 
+# Code that will activate the red light when invalid user input is given (invalid spelling error, unknown command)
+
+    # Print help message to the terminal when invalid input is given 
+    # "Invalid Input given, command is either unknown or spelled incorrectly. Please give a valid input"
+
+    # Code to play a sound when red light activates
 
 # Code that will consistently run the blue right while the program is active
 
+    # Print message to the terminal intermittently saying the program is in "Idle Mode"
+
+    # Code to play a sound when blue light is active
+
+# Code that will activate the yellow light when someone pushes the button
+     
+     # Print message to the terminal "Are you sure you want to terminate the program?"
+
+     # Yellow Light Code stops running if answer is not given in time, but if button is clicked again, program stops
 
 
