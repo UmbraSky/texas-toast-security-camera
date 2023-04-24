5d24bc1d6b62163a5e38f365ac7bd7f32bea75c4
Austin Newsom <agn019@latech.edu>
2023-04-24 14:21:06 -0500

    Austin's Setup for personal branch

--- starting_file.py
+++ starting_file.py
@@ -4,3 +4,22 @@
 # Date: 4/19/2023
 # Description: Hidden Security Camera
 ###################################################################################
+
+# Austin Newsom
+# Circuitry Component for PiCamera 
+
+# Notes to Self: Create Diagram for Circuit 
+
+
+# Code that will activate the green light whenever the program begins running
+
+
+
+# Code that will activate the red light when invalid user input is given
+
+
+
+# Code that will consistently run the blue right while the program is active
+
+
+
