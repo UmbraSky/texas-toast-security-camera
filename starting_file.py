e07a56e0640cde03f66d64ecdffc522534c144ad
UmbraSky <rks023@email.latech.edu>
2023-04-27 23:50:11 -0500

    Telegram bot fully working, simple flag system implemented for now

--- starting_file.py
+++ starting_file.py
@@ -4,4 +4,8 @@
 # Date: 4/19/2023
 # Description: Hidden Security Camera
 ###################################################################################
+import TTSecurityCam_bot
 
+ALERT_SYSTEM_FLAG = False
+if ALERT_SYSTEM_FLAG == True:
+    TTSecurityCam_bot
