9d3ba9dc66d84e5f4c484f696970cdbfa842af7a
Elora A. Browning <elora.browning@gmail.com>
2023-04-28 13:34:08 -0500

    connected all routes to files

--- app.py
+++ app.py
@@ -6,7 +6,19 @@ app.secret_key = "jlsakdjflk;asjdfllhi"
 
 @app.route("/")
 def main():
-    return "flask app is running"
+    return render_template("GUI.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
+
+@app.route("/test_error_<id>")
+def error(id):
+    return render_template("error.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1")
