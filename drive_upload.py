4cad17510758c2a3b431042add361151d386f584
UmbraSky <rks023@email.latech.edu>
2023-05-01 15:38:38 -0500

    Drive integration working, need to automate

--- drive_upload.py
+++ drive_upload.py
@@ -3,9 +3,10 @@ from pydrive.drive import GoogleDrive
 gauth = GoogleAuth()           
 drive = GoogleDrive(gauth)  
 
-upload_file_list = ['code.png']
-for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'parents': [{'id': '1pzschX3uMbxU0lB5WZ6IlEEeAUE8MZ-t'}]})
-	# Read file and set it as the content of this instance.
-	gfile.SetContentFile(upload_file)
-	gfile.Upload() # Upload the file.
+def file_upload(file):
+	upload_file_list = [file]
+	for upload_file in upload_file_list:
+		gfile = drive.CreateFile({'parents': [{'id': '1zrRvrvKqUECI5qbNQHGbJNUXOjS1z5p7'}]})
+		# Read file and set it as the content of this instance.
+		gfile.SetContentFile(upload_file)
+		gfile.Upload() # Upload the file.
